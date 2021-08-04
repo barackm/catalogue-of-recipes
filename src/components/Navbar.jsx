57e39f222db110efc911ee8bc179dcde1fd12858
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-08-04 22:30:20 +0200

    Complete the mobile version of the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,34 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IconContext } from 'react-icons';
 import { Link } from 'react-router-dom';
 import { HiPlus } from 'react-icons/hi';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { GrClose } from 'react-icons/gr';
 
-const Navbar = () => (
-  <div className="main-navbar-container d-flex">
-    <Link to="/" className="nav-logo-wrapper d-flex flex-center">
-      Food
-      <IconContext.Provider value={{ className: 'logo-plus-icon' }}>
-        <HiPlus />
-      </IconContext.Provider>
-    </Link>
-    <nav className="d-flex flex-center flex-unit nav-links-wrapper">
-      <ul className="d-flex flex-center flex-between">
-        <div className="d-flex flex-center links">
-          <li><Link to="/">Shop</Link></li>
-          <li><Link to="/">Features</Link></li>
-          <li><Link to="/">Reciples</Link></li>
-        </div>
-        <div className="login-info d-flex">
-          <li>
-            <Link to="/" className="login-btn">Login</Link>
-          </li>
-          <li>
-            <Link to="/" className="signup-btn">Sign Up</Link>
-          </li>
-        </div>
-      </ul>
-    </nav>
-  </div>
-);
+const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
+  const toggleMenu = () => setShowMenu(!showMenu);
+  return (
+    <div className={showMenu ? 'main-navbar-container d-flex open' : 'main-navbar-container d-flex'}>
+      <Link to="/" className="nav-logo-wrapper d-flex flex-center">
+        Food
+        <IconContext.Provider value={{ className: 'logo-plus-icon' }}>
+          <HiPlus />
+        </IconContext.Provider>
+      </Link>
+      <button type="button" className={showMenu ? 'navbar-overlay visible' : 'navbar-overlay'} onClick={toggleMenu}> </button>
+      <nav className="d-flex flex-center flex-unit nav-links-wrapper">
+        <ul className="d-flex flex-center flex-between links-container">
+          <div className="d-flex flex-center links">
+            <li><Link to="/">Shop</Link></li>
+            <li><Link to="/">Features</Link></li>
+            <li><Link to="/">Reciples</Link></li>
+          </div>
+          <div className="login-info d-flex">
+            <li>
+              <Link to="/" className="login-btn">Login</Link>
+            </li>
+            <li>
+              <Link to="/" className="signup-btn">Sign Up</Link>
+            </li>
+          </div>
+          <button type="button" className="close-menu-btn menu-closed" onClick={toggleMenu}>
+            <IconContext.Provider value={{ className: 'close-menu-icon' }}>
+              <GrClose />
+            </IconContext.Provider>
+          </button>
+        </ul>
+        <button type="button" className="close-menu-btn" onClick={toggleMenu}>
+          <IconContext.Provider value={{ className: 'close-menu-icon' }}>
+            <GiHamburgerMenu />
+          </IconContext.Provider>
+        </button>
+      </nav>
+    </div>
+  );
+};
 
 export default Navbar;
