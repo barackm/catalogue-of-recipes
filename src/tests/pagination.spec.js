28947eea6923f80c64bb5d5a258a3bb0947976f7
Barack Mukelenga <barackmukelenga100@gmail.com>
2021-08-05 10:47:03 +0200

    Add tests with react-testing-library to make sure the app does not have issues

--- src/tests/pagination.spec.js
+++ src/tests/pagination.spec.js
@@ -1,4 +1,5 @@
 import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
 
 import Pagination from '../components/Pagination';
 
@@ -15,5 +16,56 @@ describe('Pagination', () => {
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it('should display the right page numbers', () => {
+    render(
+      <Pagination
+        itemsCount={30}
+        pageSize={6}
+        currentPage={1}
+        onPageChange={() => {}}
+      />,
+    );
+    const pageOne = screen.getByText(/1/i);
+    expect(pageOne).toBeInTheDocument();
+  });
+
+  it('should render the page 1 as active', () => {
+    render(
+      <Pagination
+        itemsCount={30}
+        pageSize={6}
+        currentPage={1}
+        onPageChange={() => {}}
+      />,
+    );
+    const pageOne = screen.getByText(/1/i);
+    expect(pageOne).toHaveClass('active');
+  });
+
+  it('should render the page 2 as active', () => {
+    render(
+      <Pagination
+        itemsCount={30}
+        pageSize={6}
+        currentPage={2}
+        onPageChange={() => {}}
+      />,
+    );
+    const pageOne = screen.getByText(/2/i);
+    expect(pageOne).toHaveClass('active');
+  });
+
+  it('should return empty string is the itemsCount is less', () => {
+    render(
+      <Pagination
+        itemsCount={1}
+        pageSize={6}
+        currentPage={2}
+        onPageChange={() => {}}
+      />,
+    );
+    expect(() => screen.getByText(/2/i)).toThrow();
   });
 });
