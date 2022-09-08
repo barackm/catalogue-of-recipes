c41a58358e671b40471a6dd8296f26d0292abe64
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-09-08 05:33:05 +0200

    Adding all informations to the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 import { IconContext } from 'react-icons';
 import { HiPlus } from 'react-icons/hi';
 import { FaGithub, FaLinkedin, FaTwitterSquare } from 'react-icons/fa';
-import { GrEmoji } from 'react-icons/gr';
+// import { GrEmoji } from 'react-icons/gr';
 
 const Footer = () => (
   <footer className="d-flex flex-center flex-between footer-main-container">
@@ -14,14 +14,22 @@ const Footer = () => (
       </IconContext.Provider>
     </Link>
     <p className="footer-attribution d-flex flex-center">
-      &copy;
-      {' '}
-      {new Date().getFullYear()}
-      , Built with ❤️
-      {' '}
-      <a href="https://barackm.me" target="_blank" rel="noreferrer">
-        Barack Mukelenga
-      </a>
+      <span>
+        &copy;
+        {new Date().getFullYear()}
+        , Built with  ❤️
+        {' '}
+      </span>
+      <div className="d-flex">
+        <a href="https://barackm.me" target="_blank" rel="noreferrer">
+          @Barack_Mukelenga,
+          {' '}
+        </a>
+        <a href="https://benmukebo.me" target="_blank" rel="noreferrer">
+          {' '}
+          @Ben_Mukebo
+        </a>
+      </div>
     </p>
     <ul className="d-flex footer-links-wrapper footer-links">
       <li>
@@ -38,7 +46,7 @@ const Footer = () => (
           </IconContext.Provider>
         </a>
       </li>
-      <li>
+      <li className="twitter">
         <a href="https://twitter.com/BarackMukelenga" target="_blank" rel="noreferrer">
           <IconContext.Provider value={{ className: 'footer-github-icon' }}>
             <FaTwitterSquare />
@@ -47,8 +55,30 @@ const Footer = () => (
       </li>
       <li>
         <a href="https://github.com/barackm" target="_blank" rel="noreferrer">
-          <IconContext.Provider value={{ className: 'footer-github-icon' }}>
+          {/* <IconContext.Provider value={{ className: 'footer-github-icon' }}>
             <GrEmoji />
+          </IconContext.Provider> */}
+          |
+        </a>
+      </li>
+      <li>
+        <a href="https://github.com/benmukebo" target="_blank" rel="noreferrer">
+          <IconContext.Provider value={{ className: 'footer-github-icon' }}>
+            <FaGithub />
+          </IconContext.Provider>
+        </a>
+      </li>
+      <li>
+        <a href="https://www.linkedin.com/in/kasongo-mukebo-ben/" target="_blank" rel="noreferrer">
+          <IconContext.Provider value={{ className: 'footer-github-icon' }}>
+            <FaLinkedin />
+          </IconContext.Provider>
+        </a>
+      </li>
+      <li className="twitter">
+        <a href="https://twitter.com/BenMukebo" target="_blank" rel="noreferrer">
+          <IconContext.Provider value={{ className: 'footer-github-icon' }}>
+            <FaTwitterSquare />
           </IconContext.Provider>
         </a>
       </li>
