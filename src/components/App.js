ad3fac4f52adc32804c2f3f107c10268456f530d
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-09-13 20:04:48 +0200

    Add react router for login and registration

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,20 @@ import Home from '../containers/Home';
 import Navbar from './Navbar';
 import RecipeDetails from '../containers/RecipleDetails';
 import Footer from './Footer';
+import LandingPage from './landingPage/LandingPage';
+import Login from './Auth/Login';
+import Register from './Auth/Register';
 
 const App = () => (
   <div className="ppp">
     <Navbar />
     <Switch>
-      <Route path="/recipes/:id" component={RecipeDetails} />
       <Route path="/recipes" exact component={Home} />
-      <Redirect from="/" to="/recipes" />
+      <Route path="/recipes/:id" component={RecipeDetails} />
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Register} />
+      <Redirect from="/*" to="/recipes" />
       <Redirect to="/" />
     </Switch>
     <Footer />
---
+++ src/components/Auth/Login.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Login = () => (
+  <div>Login</div>
+);
+
+export default Login;
+
+// rfce
+// rafce
