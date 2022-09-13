283d676d55d132b0aed7a6ddd018d83cb0bb16f0
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-09-13 21:52:38 +0200

    Create the login and signup pages

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,7 +1,54 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaUser } from 'react-icons/fa';
+import { HiOutlineMailOpen } from 'react-icons/hi';
+import { RiLockPasswordLine } from 'react-icons/ri';
+import styles from '../../assets/scss/Login.module.scss';
 
 const Login = () => (
-  <div>Login</div>
+  <section className={styles['login-section']}>
+    <div className={styles.loginIcon}><FaUser /></div>
+    <h2>Welcome back</h2>
+
+    <form>
+      <h4>Login</h4>
+      <div className={styles['form-group']}>
+        <span className={styles.icon}><HiOutlineMailOpen /></span>
+        <input
+          type="email"
+          className="form-control"
+          id="Email1"
+          name="email"
+          placeholder="Useremail"
+          required
+          value="email"
+        />
+      </div>
+
+      <div className={styles['form-group']}>
+        <span className={styles.icon}><RiLockPasswordLine /></span>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          name="password"
+          placeholder="Password"
+          required
+          // onChange={(e) => setPassowrd(e.target.value)}
+          // value={password}
+        />
+      </div>
+
+      <button type="submit">Submit</button>
+    </form>
+    <p>
+      New here?
+      {' '}
+      <Link to="/signup" style={{ textDecoration: 'none' }}>
+        Sign Up
+      </Link>
+    </p>
+  </section>
 );
 
 export default Login;
--- src/components/Auth/Register.js
+++ src/components/Auth/Register.js
@@ -1,7 +1,80 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FaRegUser, FaUser } from 'react-icons/fa';
+import { HiOutlineMailOpen } from 'react-icons/hi';
+import { FiPhoneCall } from 'react-icons/fi';
+import { RiLockPasswordLine } from 'react-icons/ri';
+import styles from '../../assets/scss/Login.module.scss';
 
 const Register = () => (
-  <div>Register</div>
+  <section className={styles['login-register']}>
+    <div className={styles.loginIcon}><FaUser /></div>
+    <h2>Welcome</h2>
+    <form>
+      <h4>Sign Up</h4>
+      <div className={styles['form-group']}>
+        <span className={styles.icon}><FaRegUser /></span>
+        <input
+          type="text"
+          className="form-control"
+          id="name"
+          placeholder="Enter User name"
+          required
+        //   onChange={(e) => setUserName(e.target.value)}
+        //   value={user_name}
+        />
+      </div>
+      <div className={styles['form-group']}>
+        <span className={styles.icon}><HiOutlineMailOpen /></span>
+        <input
+          type="email"
+          className="form-control"
+          id="Email1"
+          placeholder="Enter Email"
+          required
+          // onChange={(e) => setEmail(e.target.value)}
+          // value={email}
+        />
+      </div>
+      <div className={styles['form-group']}>
+        <span className={styles.icon}><FiPhoneCall /></span>
+        <input
+          type="tel"
+          className="form-control"
+          id="name"
+          placeholder="Mobile Number"
+        />
+      </div>
+      <div className={styles['form-group']}>
+        <span className={styles.icon}><RiLockPasswordLine /></span>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          placeholder="password"
+          required
+        />
+      </div>
+      <div className={styles['form-group']}>
+        <span className={styles.icon}><RiLockPasswordLine /></span>
+        <input
+          type="password"
+          className="form-control"
+          id="password"
+          placeholder="Confirm Password"
+        />
+      </div>
+      <button type="submit">Sign Up</button>
+    </form>
+    <p>
+      Already have an account?
+      {' '}
+      <Link to="/login" style={{ textDecoration: 'none' }}>
+        Login
+      </Link>
+
+    </p>
+  </section>
 );
 
 export default Register;
