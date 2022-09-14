fa1a6faa919a4cbf07173b309d81c86c9c382855
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-09-14 09:41:07 +0200

    Add onSubmit e to the form and onChange event to the input field of the login component

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -1,55 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FaUser } from 'react-icons/fa';
 import { HiOutlineMailOpen } from 'react-icons/hi';
 import { RiLockPasswordLine } from 'react-icons/ri';
 import styles from '../../assets/scss/Login.module.scss';
 
-const Login = () => (
-  <section className={styles['login-section']}>
-    <div className={styles.loginIcon}><FaUser /></div>
-    <h2>Welcome back</h2>
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassowrd] = useState('');
 
-    <form>
-      <h4>Login</h4>
-      <div className={styles['form-group']}>
-        <span className={styles.icon}><HiOutlineMailOpen /></span>
-        <input
-          type="email"
-          className="form-control"
-          id="Email1"
-          name="email"
-          placeholder="Useremail"
-          required
-          value="email"
-        />
-      </div>
+  const handleLogin = (e) => {
+    e.preventDefault();
+    console.log('LOGIN', e);
+  };
 
-      <div className={styles['form-group']}>
-        <span className={styles.icon}><RiLockPasswordLine /></span>
-        <input
-          type="password"
-          className="form-control"
-          id="password"
-          name="password"
-          placeholder="Password"
-          required
-          // onChange={(e) => setPassowrd(e.target.value)}
-          // value={password}
-        />
-      </div>
+  return (
+    <section className={styles['login-section']}>
+      <div className={styles.loginIcon}><FaUser /></div>
+      <h2>Welcome back</h2>
 
-      <button type="submit">Submit</button>
-    </form>
-    <p>
-      New here?
-      {' '}
-      <Link to="/signup" style={{ textDecoration: 'none' }}>
-        Sign Up
-      </Link>
-    </p>
-  </section>
-);
+      <form onSubmit={handleLogin}>
+        <h4>Login</h4>
+        <div className={styles['form-group']}>
+          <span className={styles.icon}><HiOutlineMailOpen /></span>
+          <input
+            type="email"
+            className="form-control"
+            id="email"
+            name="Email"
+            placeholder="Useremail"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+        </div>
+
+        <div className={styles['form-group']}>
+          <span className={styles.icon}><RiLockPasswordLine /></span>
+          <input
+            type="password"
+            className="form-control"
+            id="password"
+            name="Password"
+            placeholder="Password"
+            required
+            onChange={(e) => setPassowrd(e.target.value)}
+            value={password}
+          />
+        </div>
+
+        <button type="submit">Submit</button>
+      </form>
+      <p>
+        New here?
+        {' '}
+        <Link to="/signup" style={{ textDecoration: 'none' }}>
+          Sign Up
+        </Link>
+      </p>
+    </section>
+  );
+};
 
 export default Login;
 
