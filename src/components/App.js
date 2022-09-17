15f2460724f4c9b2cd7b0257ec06ab9288f7da0e
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-09-18 00:00:32 +0200

    Update the routes inside my app and add more functionalities

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,7 @@
-import { Switch, Route, Redirect } from 'react-router-dom';
+/* eslint-disable no-unused-vars */
+import {
+  Routes, Route, Navigate, Outlet,
+} from 'react-router-dom';
 import Home from '../containers/Home';
 import Navbar from './Navbar';
 import RecipeDetails from '../containers/RecipleDetails';
@@ -7,19 +10,28 @@ import LandingPage from './landingPage/LandingPage';
 import Login from './Auth/Login';
 import Register from './Auth/Register';
 
+const SidebarLayout = () => (
+  <>
+    <Navbar />
+    <Outlet />
+    <Footer />
+  </>
+);
+
 const App = () => (
   <div className="ppp">
-    <Navbar />
-    <Switch>
-      <Route path="/recipes" exact component={Home} />
-      <Route path="/recipes/:id" component={RecipeDetails} />
-      <Route exact path="/" component={LandingPage} />
-      <Route path="/login" component={Login} />
-      <Route path="/signup" component={Register} />
-      <Redirect from="/*" to="/recipes" />
-      <Redirect to="/" />
-    </Switch>
-    <Footer />
+    {/* <Navigate to="/" /> */}
+    <Routes>
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Register />} />
+      <Route path="*" element={<Navigate to="/recipes" replace />} />
+      <Route element={<SidebarLayout />}>
+        <Route path="/recipes" exact element={<Home />} />
+        <Route path="/recipes/:id" element={<RecipeDetails />} />
+        <Route exact path="/" element={<LandingPage />} />
+      </Route>
+    </Routes>
+    {/* <Navigate from="/" to="/recipes" /> */}
   </div>
 );
 
