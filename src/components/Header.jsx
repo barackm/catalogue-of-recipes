ad626fb4ccad4fc0eff1e0321d12c477a95ee0d6
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-09-17 18:49:02 +0200

    Create the current user state and display the his username after the get axios request

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,45 @@
-import React from 'react';
+/* eslint-disable camelcase */
+/* eslint-disable no-unused-vars */
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { IconContext } from 'react-icons';
 import { BsArrowRight } from 'react-icons/bs';
-
 import { Link } from 'react-router-dom';
+import axios from 'axios';
+// import jwt_decode from 'jwt-decode';
 import illustration from '../assets/img/hero-illustration.png';
 
 const Header = (props) => {
   const {
     title1, title2, paragraph, image,
   } = props;
+
+  const [currentUser, setCurrentUser] = useState('');
+  const baseUrl = 'https://sweetaromas.herokuapp.com';
+
+  const handleRequest = async () => {
+    const config = {
+      headers: {
+        Authorization: localStorage.getItem('token'),
+      },
+    };
+
+    try {
+      const response = await axios.get(`${baseUrl}/users/5`, config);
+      const { data } = response;
+      // console.log('Show', data);
+      // const decoded = jwt_decode.decode(localStorage.getItem('token'), { header: true });
+      // console.log('hello here:', decoded);
+
+      setCurrentUser(data.user_name);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    handleRequest();
+  }, []);
   return (
     <header className="app-header">
       <div className="header-main-container d-flex flex-center">
@@ -21,12 +51,20 @@ const Header = (props) => {
               {title2}
             </h1>
             <p className="hero-paragraph">{paragraph}</p>
-            <Link to="/" className="how-they-work d-flex flex-center">
+            <Link to="/recipies" className="how-they-work d-flex flex-center">
               How they work
               <IconContext.Provider value={{ className: 'hero-arrow-icon' }}>
                 <BsArrowRight />
               </IconContext.Provider>
             </Link>
+          </div>
+          <div className="current-user">
+            <p>
+              Welcome
+              {' ~ '}
+              <span>{ currentUser }</span>
+              {'. '}
+            </p>
           </div>
           <div className="hero-image-wrapper">
             <img src={image} alt="hiro" className="hiro-image" />
