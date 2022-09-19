a169c112c33c9d80014153c4938a02fb60f9c812
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-09-19 17:20:47 +0200

    Use correctly  detail s, loading, error  reducers and loadDetailsAsync action to load the detailsRecipe page

--- src/containers/RecipleDetails.jsx
+++ src/containers/RecipleDetails.jsx
@@ -1,21 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useEffect } from 'react';
 import { IconContext } from 'react-icons';
 import { BsCheckCircle } from 'react-icons/bs';
-import { connect } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { useNavigate, useParams } from 'react-router-dom';
+import { HashLoader } from 'react-spinners';
 import Header from '../components/Header';
 import cooker from '../assets/img/cooker2.png';
+import { loadDetailsAsync } from '../store/actions/index';
 
-function RecipeDetails(props) {
-  const [recipe, setRecipe] = useState({});
-  const { allRecipes, history, match } = props;
-  const { id } = match.params;
+function RecipeDetails() {
+  const params = useParams();
+  const { id } = params;
+  const navigate = useNavigate();
+
+  const { details: recipe, loading, error } = useSelector((state) => state.recipeDetails);
+  const dispatch = useDispatch();
+
   useEffect(() => {
-    const foundid = id;
-    if (!foundid) return history.replace('/');
-    const selectedRecipe = allRecipes.find((r) => r.idMeal === foundid);
-    if (!selectedRecipe) return history.replace('/');
-    return setRecipe(selectedRecipe);
+    dispatch(loadDetailsAsync(id, navigate));
+    // const foundid = id;
+    // // if (!foundid) return navigate('/recipes');
   }, []);
 
   const myIngredients = [];
@@ -24,66 +28,65 @@ function RecipeDetails(props) {
     : ''));
 
   return (
-    <div className="recipes-details-main-container">
-      <Header image={cooker} title1={`${recipe.strMeal} 👨‍🍳 🍽️`} title2={`From: ${recipe.strArea}`} />
-      <div className="recipe-details-content-wrapper d-flex flex-unit">
-        <div className="recipe-image-details">
-          <div className="recipe-image-wrapper">
-            <img
-              src={recipe.strMealThumb}
-              alt="recipe"
-            />
-          </div>
-          <div className="recipe-ingredients">
-            <h1>Ingredients</h1>
-
-            <ul className="ingredients-list">
-              {myIngredients.map((ing) => (
-                <li key={ing.id}>
-                  <IconContext.Provider value={{ className: 'recipe-details-icon' }}>
-                    <BsCheckCircle />
-                  </IconContext.Provider>
-                  {ing.name}
-                </li>
-              ))}
-            </ul>
-          </div>
-        </div>
-        <div className="recipe-instructions-details">
-          <h1>Instructions</h1>
-          <p>
-            {recipe.strInstructions}
-          </p>
-          <div className="imbeded-video-wrapper">
-            <iframe
-              src={`https://www.youtube.com/embed/${recipe.strYoutube && recipe.strYoutube.split('=')[1]}`}
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            />
-          </div>
-        </div>
+    (error || !recipe) ? (
+      <div className="d-flex flex-center list-empty-message">
+        😥
+        {error || 'Sorry this recipes does not content any information.' }
       </div>
-    </div>
+    ) : (
+      <div className="recipes-details-main-container">
+        { loading && (
+        <div className="loading-spinner-wrapper d-flex flex-center">
+          <HashLoader color="#e0aea6" loading={loading} size={70} />
+        </div>
+        )}
+        {!loading && (
+        <>
+          <Header image={cooker} title1={`${recipe.strMeal} 👨‍🍳 🍽️`} title2={`From: ${recipe.strArea}`} />
+          <div className="recipe-details-content-wrapper d-flex flex-unit">
+            <div className="recipe-image-details">
+              <div className="recipe-image-wrapper">
+                <img
+                  src={recipe.strMealThumb}
+                  alt="recipe"
+                />
+              </div>
+              <div className="recipe-ingredients">
+                <h1>Ingredients</h1>
+
+                <ul className="ingredients-list">
+                  {myIngredients.map((ing) => (
+                    <li key={ing.id}>
+                      <IconContext.Provider value={{ className: 'recipe-details-icon' }}>
+                        <BsCheckCircle />
+                      </IconContext.Provider>
+                      {ing.name}
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            </div>
+            <div className="recipe-instructions-details">
+              <h1>Instructions</h1>
+              <p>
+                {recipe.strInstructions}
+              </p>
+              <div className="imbeded-video-wrapper">
+                <iframe
+                  src={`https://www.youtube.com/embed/${recipe.strYoutube && recipe.strYoutube.split('=')[1]}`}
+                  title="YouTube video player"
+                  frameBorder="0"
+                  allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                  allowFullScreen
+                />
+              </div>
+            </div>
+          </div>
+        </>
+        )}
+      </div>
+    )
   );
 }
 
-RecipeDetails.defaultProps = {
-  history: {},
-  match: {
-    params: { id: '11' },
-  },
-};
-
-RecipeDetails.propTypes = {
-  allRecipes: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
-  history: PropTypes.objectOf(PropTypes.any),
-  match: PropTypes.objectOf(PropTypes.any),
-};
-
-const mapStateToProps = (state) => ({
-  allRecipes: state.recipes.list,
-});
-
-export default connect(mapStateToProps)(RecipeDetails);
+export default RecipeDetails;
