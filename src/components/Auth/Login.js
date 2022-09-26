1f5a156f190522c7f9c7535573d8258e5a7086c9
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-09-26 07:42:54 +0200

    Create a login component for the user authentication

--- src/components/Auth/Login.js
+++ src/components/Auth/Login.js
@@ -34,25 +34,23 @@ const Login = () => {
   return (
     <section className={styles['login-section']}>
       <div className={styles.loginIcon}><FaUser /></div>
-      <h2>Welcome back</h2>
-
+      <h2>Sign In</h2>
       <form onSubmit={handleLogin}>
-        <h4>Login</h4>
-        <div className={styles['form-group']}>
+        <div className={styles['div-input']}>
           <span className={styles.icon}><HiOutlineMailOpen /></span>
           <input
             type="email"
             className="form-control"
             id="email"
             name="Email"
-            placeholder="Useremail"
+            placeholder="Email"
             required
             onChange={(e) => setEmail(e.target.value)}
             value={email}
           />
         </div>
 
-        <div className={styles['form-group']}>
+        <div className={styles['div-input']}>
           <span className={styles.icon}><RiLockPasswordLine /></span>
           <input
             type="password"
@@ -65,16 +63,25 @@ const Login = () => {
             value={password}
           />
         </div>
+        <div className={styles['div-checked']}>
+          <input
+            type="checkbox"
+            className="w-auto mt-0"
+            name="rememberUser"
+          />
+          <span>Remember me?</span>
+        </div>
 
-        <button type="submit">Submit</button>
+        <button type="submit">Login Now</button>
       </form>
-      <p>
-        New here?
-        {' '}
-        <Link to="/signup" style={{ textDecoration: 'none' }}>
-          Sign Up
+      <div className={styles['div-links']}>
+        <Link to="/password" className={styles.password}>
+          Forgot password?
         </Link>
-      </p>
+        <Link to="/signup" className={styles.signup}>
+          Create an acount?
+        </Link>
+      </div>
     </section>
   );
 };
