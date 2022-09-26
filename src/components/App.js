e02914bc68cf12c70acd22897a8773783971bfa6
Ben Mukebo <l.benkasmukebo7@gmail.com>
2022-09-26 08:38:28 +0200

    Modify the app router to display nav and footer on the login and signup pages

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@
 import {
   Routes, Route, Navigate, Outlet,
 } from 'react-router-dom';
+import { useState } from 'react';
 import Home from '../containers/Home';
 import Navbar from './Navbar';
 import RecipeDetails from '../containers/RecipleDetails';
@@ -12,27 +13,33 @@ import Register from './Auth/Register';
 
 const SidebarLayout = () => (
   <>
-    <Navbar />
+    {/* <Navbar /> */}
     <Outlet />
-    <Footer />
+    {/* <Footer /> */}
   </>
 );
 
-const App = () => (
-  <div className="ppp">
-    {/* <Navigate to="/" /> */}
-    <Routes>
-      <Route path="/login" element={<Login />} />
-      <Route path="/signup" element={<Register />} />
-      <Route path="*" element={<Navigate to="/recipes" replace />} />
-      <Route element={<SidebarLayout />}>
-        <Route path="/recipes" exact element={<Home />} />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-        <Route exact path="/" element={<LandingPage />} />
-      </Route>
-    </Routes>
-    {/* <Navigate from="/" to="/recipes" /> */}
-  </div>
-);
+const App = () => {
+  const [showUser, setShowUser] = useState('');
+
+  return (
+    <div className="ppp">
+      {/* <Navigate to="/" /> */}
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Register />} />
+        <Route element={<SidebarLayout />}>
+          <Route path="/recipes" exact element={<Home />} />
+          <Route path="/recipes/:id" element={<RecipeDetails />} />
+          <Route exact path="/" element={<LandingPage />} />
+        </Route>
+        <Route path="*" element={<Navigate to="/recipes" replace />} />
+      </Routes>
+      {/* <Navigate from="/" to="/recipes" /> */}
+      <Footer />
+    </div>
+  );
+};
 
 export default App;
